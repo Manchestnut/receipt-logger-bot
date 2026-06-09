5a792c7451d623e0b7670ac53a5f06f51c2b3e80
Luke <luke.manongsong@gmail.com>
2026-06-10 02:34:39 +0800

    update

--- app.py
+++ app.py
@@ -6,7 +6,14 @@ import telegram_bot_api as tg
 def main():
 
     TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-    application = ApplicationBuilder().token(TOKEN).build()
+    application = (
+    ApplicationBuilder()
+    .token(TOKEN)
+    .read_timeout(30)       # Allow up to 30 seconds to read stream chunks
+    .write_timeout(30)      # Allow up to 30 seconds to write out updates
+    .connect_timeout(30)    # Allow up to 30 seconds for the initial handshake
+    .build()
+)
 
     photo_handler = MessageHandler(filters.PHOTO, tg.handle_receipt_photo)
     application.add_handler(photo_handler)
