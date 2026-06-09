2f9cc0d24aecf342c8b8675aedfeffdecd0fa335
Luke <luke.manongsong@gmail.com>
2026-06-10 02:34:57 +0800

    update

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ def main():
     TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
     application = (
     ApplicationBuilder()
-    .token(TOKEN)
-    .read_timeout(30)       # Allow up to 30 seconds to read stream chunks
-    .write_timeout(30)      # Allow up to 30 seconds to write out updates
-    .connect_timeout(30)    # Allow up to 30 seconds for the initial handshake
-    .build()
-)
+        .token(TOKEN)
+        .read_timeout(30)       # Allow up to 30 seconds to read stream chunks
+        .write_timeout(30)      # Allow up to 30 seconds to write out updates
+        .connect_timeout(30)    # Allow up to 30 seconds for the initial handshake
+        .build()
+    )
 
     photo_handler = MessageHandler(filters.PHOTO, tg.handle_receipt_photo)
     application.add_handler(photo_handler)
